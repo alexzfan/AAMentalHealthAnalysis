80c6ac914c9154adb653452f74ab2a455c585ad3
Wendy <wendylwn@hotmail.com>
2020-04-06 20:40:39 -0400

    uwu

--- AAQoL_Anal.R
+++ AAQoL_Anal.R
@@ -1,6 +1,5 @@
 library(readr)
 
-setwd("~/Desktop/Emory Compiled/Emory Y3-S2/QTM490")
-AAQoL <- read_csv("AAQOL_Survey_Monkey_Answers_Nov7_Update.csv")
+AAQoL <- read.csv("AAQoL.csv")
 
 summary(AAQoL)
