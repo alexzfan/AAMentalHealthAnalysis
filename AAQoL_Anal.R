17e6da955e981ab21d779647f2225de94f88d311
Alex Fan <alex.fan@emory.edu>
2020-04-06 17:40:35 -0700

    wow

--- AAQoL_Anal.R
+++ AAQoL_Anal.R
@@ -1,9 +1,21 @@
+## load libraries
 library(readr)
 
+## set up space
 setwd("~/Documents/GitHub/AAMentalHealthAnalysis")
 rm(list = ls())
 
+## read the csv
 AAQoL <- read_csv("AAQoL.csv")
 
-summary(AAQoL)
+summary(AAQoL$`Therapist/Counselor`)
+table(AAQoL$Psychiatrist)
+table(AAQoL$`Therapist/Counselor`)
+table(AAQoL$Disappointment)
+table(AAQoL$Shame)
+table(AAQoL$Weakness)
+table(AAQoL$Treatment)
+table(AAQoL$Counseling)
+table(AAQoL$Discrimination)
 
+prop.table(table(AAQoL$Discrimination, AAQoL$Counseling))
